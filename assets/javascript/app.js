1e38bd84bde6324ae8b3224ee192946562ea1416
Our Laptop <Our Laptop>
2016-10-27 02:46:33 -0700

    completed work on functionality with updates to css

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,122 +1,188 @@
 $(document).ready(function() {
     // creat an object with each of the questions, the answers and the correct answer
-    var time = 3;
-    var questions = {
-        question: ["what color is water?",
-            "Which State is North of CA?",
-            "What ocean is West of CA?"
+    var questions = [{
+        question: "When was the first Summer Olympics held in Los Angeles?",
+        correctAnswer: "1932",
+        allAnswers: ["1952", "1932", "1972", "1896"],
+        image: "assets/images/olympics.gif"
+    }, {
+        question: "What the first professional football team based in Los Angeles?",
+        correctAnswer: "Los Angeles Bulldogs",
+        allAnswers: ["Los Angeles Raiders",
+            "Los Angeles Bulldogs",
+            " Los Angeles Buccaneers", "Los Angeles Rams"
         ],
-        correctAnswer: ["Blue", "Oregon", "Pacific"],
-        allAnswers: [
-            ["Green", "Red", "Blue", "Purple"],
-            ["Oregon", "Florida", "Kansas", "Nevada"],
-            ["Indian", "Atlantic", "Pacfic", "Arctic"]
-        ]
-    }
+        image: "assets/images/bulldogs.gif"
+    }, {
+        question: "What is Los Angeles City Population?",
+        correctAnswer: "4 Million",
+        allAnswers: ["4 Million", "0.5 Million", "8.5 Million",
+            "2 Million"
+        ],
+        image: "assets/images/population.gif"
+    }, {
+        question: "What is original name for Los Angeles?",
+        correctAnswer: "El Pueblo de Nuestra Senora la Reina de los Ángeles de Porciuncula",
+        allAnswers: ["Los Ángeles",
+            "El Pueblo de Nuestra Senora la Reina de los Ángeles de Porciuncula",
+            "El Pueblo de la Reyna de los Ángeles",
+            "La Reina de Los Angeles"
+        ],
+        image: "assets/images/old-la.gif"
+    }]
+    var time = 15;
+    var followingQuestion = 0;
     var correctAnswers = 0;
     var incorrectAnswers = 0;
     var unanswered = 0;
+    var questionCounter;
     // at gamestart create a setTimeout() to countdown time remaining
     // at gamestart - question 1 and the four answers are loaded
     // create an on click function for each of the answers
     function gameStart() {
+        reset();
+        clearInterval();
+        clearTimeout();
         questionCounter = setInterval(remainingTime, 1000);
-        $(".question").html(questions.question[0]);
+        $(".time-remaining").html("Time Remaining: " + time +
+            " Seconds");
+        $(
+            ".correct-answer, .answers-image, .correct-answers, .incorrect-answers, .unanswered"
+        ).html("");
+        $(".question").html(questions[followingQuestion].question);
         $(".start").hide();
-        $(".answer1").show().html(questions.allAnswers[0][0]).on(
-            "click", function() {
-                checkAnswer(questions.allAnswers[0][0])
-            });
-        $(".answer2").show().html(questions.allAnswers[0][1]).on(
-            "click", function() {
-                checkAnswer(questions.allAnswers[0][1])
-            });
-        $(".answer3").show().html(questions.allAnswers[0][2]).on(
-            "click", function() {
-                checkAnswer(questions.allAnswers[0][2])
-            });
-        $(".answer4").show().html(questions.allAnswers[0][3]).on(
-            "click", function() {
-                checkAnswer(questions.allAnswers[0][3])
-            });
+        $(".answer1").show().html(questions[followingQuestion].allAnswers[
+            0]).on("click", function() {
+            clearInterval(questionCounter);
+            checkAnswer(questions[followingQuestion].allAnswers[
+                0])
+        });
+        $(".answer2").show().html(questions[followingQuestion].allAnswers[
+            1]).on("click", function() {
+            clearInterval(questionCounter);
+            checkAnswer(questions[followingQuestion].allAnswers[
+                1])
+        });
+        $(".answer3").show().html(questions[followingQuestion].allAnswers[
+            2]).on("click", function() {
+            clearInterval(questionCounter);
+            checkAnswer(questions[followingQuestion].allAnswers[
+                2])
+        });
+        $(".answer4").show().html(questions[followingQuestion].allAnswers[
+            3]).on("click", function() {
+            clearInterval(questionCounter);
+            checkAnswer(questions[followingQuestion].allAnswers[
+                3])
+        });
     }
 
     function remainingTime() {
-            time--;
             $(".time-remaining").html("Time Remaining: " + time +
                 " Seconds");
+            time--;
             // if no answer
-            if (time === 0) {
+            if (time === -1) {
                 unansweredScreen();
             }
         }
         // compare the selection against the correct answer.
 
     function checkAnswer(userSelection) {
-            console.log("Checking Answer");
-            if (userSelection === questions.correctAnswer[0] || userSelection === questions.correctAnswer[1] || userSelection === questions.correctAnswer[2]) {
-                console.log("right answer");
+            if (userSelection === questions[followingQuestion].correctAnswer) {
                 correctAnswerScreen();
-            } else if (userSelection !== questions.correctAnswer[0] || userSelection !== questions.correctAnswer[1] || userSelection !== questions.correctAnswer[2]) {
-                console.log("wrong answer");
+            } else if (userSelection !== questions[followingQuestion].correctAnswer) {
                 incorrectAnswerScreen();
             }
         }
-        // if correct answer is picked - display win for 5 seconds and add 1 to win
+        // if correct answer is picked - display win for and add 1 to win
 
     function correctAnswerScreen() {
-            console.log("correct answer screen");
-            setTimeout(gameStart, 5000);
+            setTimeout(nextQuestion, 3000);
             clearInterval(questionCounter);
+            reset();
             correctAnswers++;
             $(".time-remaining").html("Time Remaining: " + time +
                 " Seconds");
             $(".question").html("Correct!");
-            $(".answers").html(
-                '<img src="../TriviaGame/assets/images/blue-water.jpg" alt="Blue Water"/>'
-            );
+            $(".answer1, .answer2, .answer3, .answer4").hide();
+            $(".correct-answer").show().html("You got it! " + questions[
+                followingQuestion].correctAnswer) + " is right.";
+            $(".answers-image").html("<img src=" + questions[
+                followingQuestion].image + ">");
         }
-        // if incorrect answer is picked - display lose for 5 seconds and add 1 to lose
+        // if incorrect answer is picked - display lose and add 1 to lose
 
     function incorrectAnswerScreen() {
-            console.log("incorrect answer screen");
+            setTimeout(nextQuestion, 3000);
             clearInterval(questionCounter);
+            reset();
             incorrectAnswers++;
             $(".time-remaining").html("Time Remaining: " + time +
                 " Seconds");
             $(".question").html("Nope!");
-            $(".correct-answer").html("The Correct Answer was: " +
-                questions.correctAnswer[0]);
-            $(".answers").html(
-                '<img src="../TriviaGame/assets/images/blue-water.jpg" alt="Blue Water"/>'
-            );
+            $(".answer1, .answer2, .answer3, .answer4").hide();
+            $(".correct-answer").show().html("The Correct Answer was: " +
+                questions[followingQuestion].correctAnswer);
+            $(".answers-image").show().html("<img src=" + questions[
+                followingQuestion].image + ">");
         }
-        // if no answer is picked by timeout then display no answer for 5 seconds and add 1 to unanswered
+        // if no answer is picked by timeout then display no answer and add 1 to unanswered
 
     function unansweredScreen() {
-            console.log("unansweredScreen");
+            setTimeout(nextQuestion, 3000);
             clearInterval(questionCounter);
+            reset();
             unanswered++;
             $(".time-remaining").html("Time Remaining: 0 Seconds");
             $(".question").html("Out of Time!");
-            $(".correct-answer").html("The Correct Answer was: " +
-                questions.correctAnswer[0]);
-            $(".answers").html(
-                '<img src="../TriviaGame/assets/images/blue-water.jpg" alt="Blue Water"/>'
-            );
+            $(".answer1, .answer2, .answer3, .answer4").hide();
+            $(".correct-answer").show().html("The Correct Answer was: " +
+                questions[followingQuestion].correctAnswer);
+            $(".answers-image").show().html("<img src=" + questions[
+                followingQuestion].image + ">");
         }
         //  then run a nextquestion function that moves to next question in the array
+        // Once array of questions ends, then display wins, losses, unanswered and a button with onclick function to reset game
 
     function nextQuestion() {
-            console.log("next Question");
+        if (followingQuestion === questions.length - 1) {
+            lastPage();
+        } else {
+            followingQuestion++;
+            clearInterval();
+            gameStart();
         }
-        // Once array of questions ends, then display wins, losses, unanswered and a button with onclick function to reset game
-        // create a reset function to clear the scores and run the gamestart function
+    }
+
+    function lastPage() {
+        clearInterval();
+        followingQuestion = 0;
+        $(".time-remaining").html("Time Remaining: " + time +
+            " Seconds");
+        $(".question").html("All done, here\'s how you did!");
+        $(".correct-answer").hide();
+        $(".start").hide();
+        $(".answers-image").hide();
+        $(".correct-answers").html("Correct Answers: " +
+            correctAnswers);
+        $(".incorrect-answers").html("Incorrect Answers: " +
+            incorrectAnswers);
+        $(".unanswered").html("Unanswered: " + unanswered);
+        $(".answer4").hide();
+        $(".start-over").show().html("Start Over?").on("click",
+            gameStart);
+    }
+
+    function reset() {
+            time = 15;
+            $(".start-over").hide();
+        }
         // onload - create the start button to run the gamestart function
 
     function firstPage() {
         $(".start").html("Start the game!");
-        $(".answer1, .answer2, .answer3, .answer4").hide();
+        $(".answer1, .answer2, .answer3, .answer4, .start-over").hide();
         $(".start").on("click", gameStart);
     }
     firstPage();
